f9d17912fcf10a6401ce2d5232c965cb5903946d
Thiago Ferreira <thiagoferr212@gmail.com>
2024-10-21 18:02:40 -0300

    Criação da lista de palavras

--- src/model/Palavra.java
+++ src/model/Palavra.java
@@ -1,5 +1,49 @@
 package model;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Palavra {
 
+    private Set<String> listaDePalavras;
+
+    public Palavra() {
+        this.listaDePalavras = new HashSet<>(Arrays.asList("Carro", "Abelha", "Livro", "Quadro", "Caneta"));
+    }
+
+    public Set<String> getListaDePalavras() {
+        return listaDePalavras;
+    }
+
+    public void setListaDePalavras(Set<String> listaDePalavras) {
+        this.listaDePalavras = listaDePalavras;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((listaDePalavras == null) ? 0 : listaDePalavras.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Palavra other = (Palavra) obj;
+        if (listaDePalavras == null) {
+            if (other.listaDePalavras != null)
+                return false;
+        } else if (!listaDePalavras.equals(other.listaDePalavras))
+            return false;
+        return true;
+    }
+
+    
 }
